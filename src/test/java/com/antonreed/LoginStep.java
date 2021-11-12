5b912b1118c60a1774a0082b9d84a41b873049f0
admin <liveforlfc@gmail.com>
2021-11-12 17:56:04 +0300

    Commit 1

--- src/test/java/com/antonreed/LoginStep.java
+++ src/test/java/com/antonreed/LoginStep.java
@@ -2,7 +2,17 @@ package com.antonreed;
 
 public class LoginStep {
 
-    /*
-    Код Влада
-     */
+    public void restart(){
+        System.out.println("restart");
+    }
+
+    public void start(){
+        System.out.println("start");
+        System.out.println("stop");
+    }
+
+    public void stop(){
+        System.out.println("stop");
+        System.out.println("start");
+    }
 }
